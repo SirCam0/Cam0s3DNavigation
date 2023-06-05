90398b28aec61594e5c93db662ef56001fcd191a
SirCam0 <123349658+SirCam0@users.noreply.github.com>
2023-06-05 22:42:05 +0100

    Add files via upload

---
+++ Private/NavMesh3DModifier.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NavMesh3DModifier.h"
+
+ANavMesh3DModifier::ANavMesh3DModifier()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Volume = CreateDefaultSubobject<UBoxComponent>("Volume");
+	this->RootComponent = Volume;
+	Volume->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Volume->SetCollisionObjectType(ECollisionChannel::ECC_WorldStatic);
+	this->Tags.Add("NavMeshModifierVolume");
+}
+
+ANavMesh3DModifier* ANavMesh3DModifier::GetNavMesh3DModifierReference_Implementation()
+{
+	return this;
+}
+
+// Called when the game starts or when spawned
+void ANavMesh3DModifier::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANavMesh3DModifier::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
---
+++ Private/NavigationFunctionLibrary3D.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NavigationFunctionLibrary3D.h"
+#include "CubicNavMesh3D.h"
+#include <Kismet/GameplayStatics.h>
+#include <Kismet/KismetMathLibrary.h>
+#include "Cam0s3DNavigationInterface.h"
+
+TArray<FVector> UNavigationFunctionLibrary3D::GetPath3D(AActor* ActorToMove, FVector TargetLocation, bool DrawPath)
+{
+	TArray<AActor*> NavMeshes;
+	UGameplayStatics::GetAllActorsOfClass(ActorToMove, ACubicNavMesh3D::StaticClass(), NavMeshes);
+	float ClosestDistance = INFINITY;
+	ACubicNavMesh3D* ClosestNavMesh = nullptr;
+	for (AActor* NavMesh : NavMeshes) {
+		if (NavMesh->Implements<UCam0s3DNavigationInterface>()) {
+			ACubicNavMesh3D* Nav = ICam0s3DNavigationInterface::Execute_GetCubicNavMesh3DReference(NavMesh);
+			const float CurrentDistance = UKismetMathLibrary::Vector_Distance(Nav->GetActorLocation(), ActorToMove->GetActorLocation());
+			if (CurrentDistance < ClosestDistance) {
+				ClosestDistance = CurrentDistance;
+				ClosestNavMesh = Nav;
+			}
+		}
+	}
+	if (ClosestNavMesh) {
+		TArray<FVector> Path = ClosestNavMesh->GetPath(ActorToMove->GetActorLocation(), TargetLocation, DrawPath);
+		return Path;
+	}
+	UE_LOG(LogTemp, Warning, TEXT("No Nav Mesh Found for pathfinding"));
+	return {};
+}
